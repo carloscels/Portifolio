2c9b0d3023a85a6b5898af4e58eb02472aa123d0
caducels <carlos.cels.1999@gmail.com>
2023-05-01 21:35:10 -0300

    portifolio 1.0.1

--- js/script.js
+++ js/script.js
@@ -3,6 +3,17 @@ window.addEventListener("scroll", function(){
     header.classList.toggle("sticky",window.scrollY > 90)
 });
 
+let menu = document.querySelector(".menuIcon");
+let navlist = document.querySelector(".navlist");
+
+menu.onclick = ()=>{
+  navlist.classList.toggle("active");
+  header.classList.toggle("sticky", "active");
+}
+window.onscroll = () =>{
+navlist.classList.remove("active");
+}
+
 function mudarconteudo(){
     document.getElementById("conteudo").innerHTML = "Habilidades Full-Stack"
     document.getElementById("conteudo2").innerHTML = "habilidades em Html, Css, Javascript e Angular no Front-end, e Java e Python no Back-end."
